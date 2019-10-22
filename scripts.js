0d1e2e2bbcf6b3056c5cebbcb87c09cdda43d635
Student <example@example.com>
2019-10-22 13:41:04 -0700

    create button for woof and meow and button for oink and ribbit

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// $(document).ready(function() {
+//   $("button#hello").click(function() {
+//     $("ul#user").prepend("<li>Hello!</li>");
+//     $("ul#webpage").prepend("<li>Why hello there!</li>");
+//   });
+//
+//   $("button#goodbye").click(function() {
+//     $("ul#user").prepend("<li>Goodbye!</li>");
+//     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+//   });
+//
+//   $("button#stop").click(function() {
+//     $("ul#user").prepend("<li>Stop copying me!</li>");
+//     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+//   });
+// });
+
+$(document).ready(function() {
+  $("#cats").click(function() {
+    $("#cats-list").append("<li>Meow!</li>");
+    $("#dogs-list").append("<li>Woof!</li>");
+
+  });
+
+  $("#alt").click(function() {
+    $("#cats-list").prepend("<li>Oink!</li>");
+    $("#dogs-list").prepend("<li>Ribbit!</li>");
+
+  });
+
+});
